14bdf877e5355dd4a8355ed985378ca805202d79
Ori Roll <oriroll@gmail.com>
2019-12-25 11:40:33 +0200

    New context provider (App) and consumer (day), works

--- src/appContext.js
+++ src/appContext.js
@@ -7,15 +7,15 @@ function AppContextProvider (props) {
 
     //Temp to replace database
     const tasks = [
-        {id : "1", time: getTime(new Date('2018-11-28T01:00:00')), title: "Miss. T", detail: "This is very important" }
-    , {id : "2", time: getTime(new Date()), title: "Today", detail: "stuff 2 do" }
+        {id : "1", time: new Date('2018-11-28T01:00:00').getTime(), title: "Miss. T", detail: "This is very important" }
+    , {id : "2", time: new Date().getTime(), title: "Today", detail: "stuff 2 do" }
     ];
 
 
-    const [tasks, setTasks] = useState(tasks);
+    const [taskData, setTaskData] = useState(tasks);
 
     return (
-        <AppContext.Provider value={{tasks}}>
+        <AppContext.Provider value={{taskData}}>
             {props.children}
         </AppContext.Provider>
     );
--- src/components/Day.js
+++ src/components/Day.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Task from './Task.js'
+import React, { useContext } from 'react';
+import Task from './Task.js';
+import {AppContext} from 'C:/Users/Denise/Desktop/Ori - WEB/TaskCalendar/V_0.0.0/task-calendar/src/appContext.js';
 
 function Day(props) {
 
     let dayTitle = <p className="dayTitle">This is the day</p>
     let dayContent = [<p className="task">task1</p>, <p  className="task">task2</p>, <p  className="task">task3</p>]
 
-
+    const {taskData} = useContext(AppContext);
+    console.log(taskData);
     return (
         <div className="day">
             {dayTitle}
