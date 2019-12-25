9069ba02c88b68f418fb179f725fa20e7e508b31
Ori Roll <oriroll@gmail.com>
2019-12-25 15:24:46 +0200

    task now use data to displat title, dtail and formated time + defaultData

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
+const defaultData = {id : 0, time : "" , title: "empty t", detail: "empty d"}; 
+
 function Task(props) {
+
+    let data;
+
+    props.taskData ? data = props.taskData : data = defaultData;
+    
+    // This creates time format
+    let taskTime = ( ()=> { 
+        if (data.time === "") {return "no time"};
+        let theDate = new Date(data.time); 
+        return ( theDate.getHours() + ":" + theDate.getMinutes().toString().padStart(2 , "0") ) 
+        } )();
 
     return (
         <div className="task">
-            <h5 className="taskTitle">Task name</h5>
+            <h5 className="taskTitle">{data.title}</h5>
+            <p className="taskTime">{taskTime}</p>
             <p>
-                This is the tasks paragraph <br/>
-                This is the tasks paragraph <br/>
-                This is the tasks paragraph
+                {data.detail}
             </p>
         </div>
     ); 
