d14165ababf8c5b9a80a9a0b73432028181775ad
Ori Roll <oriroll@gmail.com>
2019-12-25 11:23:48 +0200

    Added "tasks" state to context

--- src/appContext.js
+++ src/appContext.js
@@ -2,10 +2,20 @@ import React, { useState } from "react";
 
 const AppContext = React.createContext();
 
+
 function AppContextProvider (props) {
 
+    //Temp to replace database
+    const tasks = [
+        {id : "1", time: getTime(new Date('2018-11-28T01:00:00')), title: "Miss. T", detail: "This is very important" }
+    , {id : "2", time: getTime(new Date()), title: "Today", detail: "stuff 2 do" }
+    ];
+
+
+    const [tasks, setTasks] = useState(tasks);
+
     return (
-        <AppContext.Provider>
+        <AppContext.Provider value={{tasks}}>
             {props.children}
         </AppContext.Provider>
     );
