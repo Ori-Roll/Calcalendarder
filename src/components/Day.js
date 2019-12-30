d11467074b73aa3c454ca19c1a846cbeea25f8ce
Ori Roll <oriroll@gmail.com>
2019-12-30 13:05:20 +0200

    Day -  date from props

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,21 +1,20 @@
 import React, { useContext } from 'react';
 import Task from './Task.js';
-import {AppContext} from 'C:/Users/Denise/Desktop/Ori - WEB/TaskCalendar/V_0.0.0/task-calendar/src/appContext.js';
+import {AppContext, getTasks} from 'C:/Users/Denise/Desktop/Ori - WEB/TaskCalendar/V_0.0.0/task-calendar/src/appContext.js';
 
 function Day(props) {
 
+    const dayDate = props.dayDate;
+    const {getTasks} = useContext(AppContext);
+    let dayTasks = getTasks();
 
-    const {taskData} = useContext(AppContext);
-    
-    let dayTitle = <p className="dayTitle">This is the day</p>
-    
-    let dayContent = [<p className="task">task1</p>, <p  className="task">task2</p>, <p  className="task">task3</p>]
+    const dayTitle = <p className="dayTitle">{dayDate}</p>
 
     return (
         <div className="day">
             {dayTitle}
-            <Task taskData = {taskData[0]} />
-            <Task taskData = {taskData[1]} />
+            <Task taskData = {dayTasks[0]} />
+            <Task taskData = {dayTasks[0]} />
             <Task/>
         </div>
     );
