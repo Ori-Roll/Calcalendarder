d9efb9e5631259ec328a207ad5b36370786e47db
Ori Roll <oriroll@gmail.com>
2020-03-09 13:55:50 +0200

    taskTime and task p style change - code more readable

--- src/components/Task.js
+++ src/components/Task.js
@@ -11,16 +11,11 @@ function Task({ taskProps = getDefaultData(), onTaskClick, onDragStartHandler })
 		if (taskProps.startDate === "") {
 			return "no startDate";
 		}
-		return (
-			theDate.getHours() +
-			":" +
-			theDate
-				.getMinutes()
-				.toString()
-				.padStart(2, "0")
-		);
+		return `${theDate.getHours()}:${theDate
+			.getMinutes()
+			.toString()
+			.padStart(2, "0")}`;
 	})();
-
 	let taskBoxStart = (() => {
 		if (taskProps.startDate === "") {
 			return 0;
@@ -48,22 +43,26 @@ function Task({ taskProps = getDefaultData(), onTaskClick, onDragStartHandler })
 		onTaskClick();
 	};
 
+	function taskTooSmallToP() {
+		return Number(taskBoxEnd.replace("px", "")) < 45 ? { opacity: "0%" } : {};
+	}
+
 	return (
 		<div
 			className='task'
 			style={{ height: taskBoxEnd, top: taskBoxStart }}
 			onClick={taskClickHandler}
 			onDragStart={e => onDragStartHandler(e, taskProps)}
-			draggable={"true"}>
-			<p className='taskTitle' style={{ color: taskProps.color, borderLeftColor: taskProps.color }}>
+			draggable='true'>
+			<p
+				className='task-title'
+				style={{ color: taskProps.color, borderLeftColor: taskProps.color }}>
 				{taskProps.title}
 			</p>
 			<p className='task-time' style={{ color: taskProps.color, borderLeftColor: taskProps.color }}>
 				{taskTime}
 			</p>
-			<p
-				className='task-description'
-				style={Number(taskBoxEnd.replace("px", "")) < 45 ? { opacity: "0%" } : {}}>
+			<p className='task-description' style={taskTooSmallToP()}>
 				{taskProps.description}
 			</p>
 		</div>
