bedef8e9ad05de4a9d0e31a7c5364fed07ec9203
Ori Roll <oriroll@gmail.com>
2020-03-09 11:48:21 +0200

    moved colorPickers colors to helpers

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -21,4 +21,37 @@ function getTaskTimeFromEvent(onDayRef, event) {
 	return { hours: fixedHour, minutes: fixedMin };
 }
 
-export { getTaskTimeFromEvent, def, getDefaultData };
+const pickerColors = [
+	"#2C3D4F",
+	"#91A79E",
+	"#F1E5C8",
+	"#EDB19F",
+	"#C87660",
+	"#D1AAA7",
+	"#C6A7D1",
+	"#A7D1C1",
+	"#BCD1A7",
+	"#615855",
+	"#555B61",
+	"#5FC6C7",
+	"#CEA296",
+	"#B87A76",
+	"#C75F7A",
+	"#C7B65F",
+	"#7682B8",
+	"#977E9E",
+	"#7E859E",
+	"#A0CE96",
+	"#4B4F2C",
+	"#703957",
+	"#472C4F",
+	"#2C334F",
+	"#A9A875",
+	"#b81639",
+	"#b87216",
+	"#dad046",
+	"gray",
+	"white"
+];
+
+export { getTaskTimeFromEvent, def, getDefaultData, pickerColors };
