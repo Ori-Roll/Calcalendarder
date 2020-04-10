5320c7b5ca33ef59851d6abb3f2a8e13108e2741
Ori Roll <oriroll@gmail.com>
2020-04-10 12:54:56 +0300

    Updates

---
+++ precache-manifest.ad54d100285f8ace60c5d8a7e1bb4cd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6b74aa6f4456e6d03cb062c8d61547",
+    "url": "/Calcalendarder/index.html"
+  },
+  {
+    "revision": "fc9b6ded750c7f73ecd5",
+    "url": "/Calcalendarder/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "a05d13be1063728741b9",
+    "url": "/Calcalendarder/static/css/main.d746e7e4.chunk.css"
+  },
+  {
+    "revision": "fc9b6ded750c7f73ecd5",
+    "url": "/Calcalendarder/static/js/2.b647aecf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Calcalendarder/static/js/2.b647aecf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a05d13be1063728741b9",
+    "url": "/Calcalendarder/static/js/main.edcf579d.chunk.js"
+  },
+  {
+    "revision": "81c03968bcd3f723fb46",
+    "url": "/Calcalendarder/static/js/runtime-main.3385b06b.js"
+  }
+]);
